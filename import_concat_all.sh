c6fe93593b78ce13f5c2d3ef480bf12b24f99db8
tolf <140963545+tolfdog@users.noreply.github.com>
2025-08-02 16:14:11 -0700

    fix up jank

--- import_concat_all.sh
+++ import_concat_all.sh
@@ -2,10 +2,20 @@
 script_dir="$(dirname -- "${BASH_SOURCE[0]:-$0}")"
 import="$script_dir/concat_importer.py"
 
-$import anthrocon.json https://reg.anthrocon.org & \
-$import las-vegas-fur-con.json https://reg.lasvegasfurcon.org & \
-$import megaplex.json https://reg.megaplexcon.org & \
-$import further-confusion.json https://reg.furtherconfusion.org & \
-$import biggest-little-fur-con.json https://reg.goblfc.org & \
-$import pawcon.json https://reg.pacanthro.org & \
-$import anthroexpo.json https://reg.anthroexpo.net
+cleanup() {
+    local exit_code=0
+    for pid in $(jobs -p); do
+        wait "$pid" || exit_code=1
+    done
+    exit $exit_code
+}
+
+trap cleanup EXIT
+
+$import anthrocon.json https://reg.anthrocon.org &
+$import las-vegas-fur-con.json https://reg.lasvegasfurcon.org &
+$import megaplex.json https://reg.megaplexcon.org &
+$import further-confusion.json https://reg.furtherconfusion.org &
+$import biggest-little-fur-con.json https://reg.goblfc.org &
+$import pawcon.json https://reg.pacanthro.org &
+$import anthroexpo.json https://reg.anthroexpo.net &
