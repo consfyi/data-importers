18f21b3d5c9718ec543cff639a9e38cbb14e4deb
tolf <140963545+tolfdog@users.noreply.github.com>
2025-08-02 16:10:15 -0700

    parallelize concat import

--- import_concat_all.sh
+++ import_concat_all.sh
@@ -2,10 +2,10 @@
 script_dir="$(dirname -- "${BASH_SOURCE[0]:-$0}")"
 import="$script_dir/concat_importer.py"
 
-$import anthrocon.json https://reg.anthrocon.org
-$import las-vegas-fur-con.json https://reg.lasvegasfurcon.org
-$import megaplex.json https://reg.megaplexcon.org
-$import further-confusion.json https://reg.furtherconfusion.org
-$import biggest-little-fur-con.json https://reg.goblfc.org
-$import pawcon.json https://reg.pacanthro.org
+$import anthrocon.json https://reg.anthrocon.org & \
+$import las-vegas-fur-con.json https://reg.lasvegasfurcon.org & \
+$import megaplex.json https://reg.megaplexcon.org & \
+$import further-confusion.json https://reg.furtherconfusion.org & \
+$import biggest-little-fur-con.json https://reg.goblfc.org & \
+$import pawcon.json https://reg.pacanthro.org & \
 $import anthroexpo.json https://reg.anthroexpo.net
